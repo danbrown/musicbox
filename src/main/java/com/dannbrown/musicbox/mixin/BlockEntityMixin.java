edafa49e0b2f140487b38f0bb9d15f271181d0ef
Daniel Brown <danielbrown25.9.2@gmail.com>
2024-07-03 11:02:06 -0300

    removed jarjar

--- src/main/java/com/dannbrown/musicbox/mixin/BlockEntityMixin.java
+++ src/main/java/com/dannbrown/musicbox/mixin/BlockEntityMixin.java
@@ -6,9 +6,6 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = {BlockEntity.class})
 public class BlockEntityMixin {
